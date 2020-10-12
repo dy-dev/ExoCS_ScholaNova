193e94f1a1145c1996e9e7f1da27187dc6bf1049
dy-dev-gitlab <dominique@arcreane.com>
2020-10-12 14:14:57 +0200

    Exercice 1, affecation de variables

---
+++ ProjetDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5, b = 6;
+        String add = "Addition";
+        String sub = "Soustraction";
+
+        Console.WriteLine("L'" + add + " entre " + a + " et " + b + " vaut " + (a + b));
+        Console.WriteLine("La " + sub  + " entre " + a + " et " + b + " vaut " + (a - b));
+        Console.ReadLine();
+    }
+}
