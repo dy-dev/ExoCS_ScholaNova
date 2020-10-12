f3253dc32a5f4c906f0d27834b499afcae69471e
dy-dev-gitlab <dominique@arcreane.com>
2020-10-12 15:20:56 +0200

    Ajout de fonctions pour addition et soustraction

--- ProjetDemo/Program.cs
+++ ProjetDemo/Program.cs
@@ -5,11 +5,27 @@ class Program
     static void Main(string[] args)
     {
         int a = 5, b = 6;
-        String add = "Addition";
-        String sub = "Soustraction";
 
-        Console.WriteLine("L'" + add + " entre " + a + " et " + b + " vaut " + (a + b));
-        Console.WriteLine("La " + sub  + " entre " + a + " et " + b + " vaut " + (a - b));
+        Addition(a, b);
+        Soustraction(a, b);
         Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Fonction d'addition entre 2 variable
+    /// </summary>
+    /// <param name="val1">Première variable qui sera additionné</param>
+    /// <param name="val2">Deuxième variable servant dans l'opération</param>
+    static void Addition(int val1, int val2)
+    {
+        String add = "Addition";
+        Console.WriteLine("L'" + add + " entre " + val1 + " et " + val2 + " vaut " + (val1 + val2));
+
+    }
+
+    static void Soustraction(int val1, int val2)
+    {
+        String sub = "Soustraction";
+        Console.WriteLine("La " + sub + " entre " + val1 + " et " + val2 + " vaut " + (val1 - val2));
     }
 }
